afa0ec5dede9c7300bfb17c18b1d71e56a7ca2c1
Capel Howorth <capelhoworth@gmail.com>
2024-02-16 13:21:58 -0500

    all dependencies added. and html updated

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-/* Surprise! You probably thought you could just forget
-the line of code you just learned! Nope, not on my watch!
+// JSX = "a flavor of JavaScript that looks like HTML"
 
-Try to write that 1-liner of React code again! This time,
-see if you can figure out how to render an <ul> with 2+ <li>s inside*/
+
 ReactDOM.render(
-    <ul><li>Thing 1</li><li>Thing 2</li></ul>,
+    <h1 className="header">This is JSX</h1>, 
     document.getElementById("root")
 )
+
