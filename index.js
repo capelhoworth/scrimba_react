cd1e0beda17e4a1ec137c6fcfbdb01e3312c97fa
Capel Howorth <capelhoworth@gmail.com>
2024-02-16 13:34:24 -0500

    finish phase1

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 // JSX = "a flavor of JavaScript that looks like HTML"
 
+// const page = (
+//     <div>
+//         <h1 className="header">This is JSX</h1>
+//         <p>This is a paragraph</p>
+//     </div> 
+// )
+
+const navbar = (
+    <nav>
+        <h1>Website Brand Name</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>  
+)
 
 ReactDOM.render(
-    <h1 className="header">This is JSX</h1>, 
+    navbar,
     document.getElementById("root")
 )
 
